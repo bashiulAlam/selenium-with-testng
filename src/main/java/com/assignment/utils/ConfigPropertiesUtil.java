ecb6f58f235cb50e7c32cc029e1651459451caed
Bashiul Alam Sabab <bashiul.sabab@kebt.co.kr>
2021-06-25 18:27:31 +0600

    test insurance project

--- src/main/java/com/assignment/utils/ConfigPropertiesUtil.java
+++ src/main/java/com/assignment/utils/ConfigPropertiesUtil.java
@@ -3,6 +3,7 @@ package com.assignment.utils;
 import java.util.Properties;
 
 public class ConfigPropertiesUtil {
+    //facebook assignment config variables
     public static String GOOGLE_URL;
     public static String FACEBOOK_URL;
 
@@ -11,21 +12,33 @@ public class ConfigPropertiesUtil {
     public static String USER_EMAIL;
     public static String PASSWORD;
 
+    //test insurance company assignment variables
+    public static String TEST_INSURANCE_COMPANY_URL;
+
     public interface ConfigElements {
+        //facebook elements
         static final String GOOGLE_URL = "GOOGLE";
         static final String FACEBOOK_URL = "FACEBOOK";
         static final String POST_TEXT = "POST_TEXT";
         static final String USER_EMAIL = "USER_EMAIL";
         static final String PASSWORD = "PASSWORD";
+
+        //test insurance company elements
+        static final String TEST_INSURANCE_COMPANY_URL = "TEST_INSURANCE_COMPANY_URL";
     }
 
     static {
+        //load property file
         Properties configProperties = Configuration.loadProperties(Configuration.PROPERTY_FILE_NAME);
 
+        //assign facebook assignment variables
         GOOGLE_URL = configProperties.getProperty(ConfigElements.GOOGLE_URL);
         FACEBOOK_URL = configProperties.getProperty(ConfigElements.FACEBOOK_URL);
         POST_TEXT = configProperties.getProperty(ConfigElements.POST_TEXT);
         USER_EMAIL = configProperties.getProperty(ConfigElements.USER_EMAIL);
         PASSWORD = configProperties.getProperty(ConfigElements.PASSWORD);
+
+        //assign test insurance company assignment variables
+        TEST_INSURANCE_COMPANY_URL = configProperties.getProperty(ConfigElements.TEST_INSURANCE_COMPANY_URL);
     }
 }
